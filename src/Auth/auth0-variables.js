fbb9a8405d9e79095b70b4be22b1377c79e126c7
DarryllRobinson <darryll@stillproud.com>
2018-09-24 11:17:38 +0200

    first commit

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'fcmcms.eu.auth0.com',
+  clientId: 'W9w3YzgENsp1zkT40TAUr7VxYVcyj6iN',
+  //callbackUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/callback' : 'https://stillproud.com/callback',
+  callbackUrl: window.location.origin + '/callback'
+}
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        Card will go here
+      </div>
+    );
+  }
+}
+
+export default Card;
